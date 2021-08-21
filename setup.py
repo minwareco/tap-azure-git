c8c2e561c712d83f18bc00bf073daa722d6adb34
Kevin Borders <kevin@collage.com>
2021-08-21 16:29:00 -0400

    adding initial files from github tap as a starting point

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tap-azure-git',
+      version='0.1',
+      description='Singer tap for Azure DevOps API Git data',
+      author='minWare',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_azure_git'],
+      install_requires=[
+          'singer-python==5.12.1',
+          'requests==2.20.0'
+      ],
+      extras_require={
+          'dev': [
+              'pylint',
+              'ipdb',
+              'nose',
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-azure-git=tap_azure_git:main
+      ''',
+      packages=['tap_azure_git'],
+      package_data = {
+          'tap_azure_git': ['tap_azure_git/schemas/*.json']
+      },
+      include_package_data=True
+)
