88686d1163b7c2994ed9f8f5d429ff3b2b235c63
Myles Henderson <myles@minware.com>
2024-12-11 13:20:28 -0500

    suggestion (#27)
    * suggestion
    * update

--- tap_azure_git/__init__.py
+++ tap_azure_git/__init__.py
@@ -1049,7 +1049,7 @@ def get_all_pipelines(schema, org, repo_path, state, mdata, start_date):
                 pipeline = {
                     **raw_pipeline,
                     '_sdc_repository': sdc_repository,
-                    '_sdc_id': '{}/pipeline/{}/{}'.format(sdc_repository, raw_pipeline['id'], raw_pipeline['revision'])
+                    '_sdc_id': '{}/pipeline/{}'.format(sdc_repository, raw_pipeline['id'])
                 }
                 
                 with singer.Transformer() as transformer:
@@ -1106,7 +1106,8 @@ def get_all_pipeline_runs(schema, org, repo_path, pipeline, state, mdata, start_
             run = {
                 **raw_run,
                 '_sdc_repository': sdc_repository,
-                '_sdc_id': '{}/pipeline/{}/run/{}'.format(sdc_repository, pipeline['id'], raw_run['id'])
+                '_sdc_id': '{}/run/{}'.format(sdc_repository, raw_run['id']),
+                '_sdc_parent_id': '{}/pipeline/{}'.format(sdc_repository, pipeline['id'])
             }
             
             with singer.Transformer() as transformer:
@@ -1150,8 +1151,8 @@ def get_build_timeline(schema, org, repo_path, build, state, mdata, start_date):
         build_timeline = {
             **raw_build_timeline,
             '_sdc_repository': sdc_repository,
-            '_sdc_id': '{}/build/{}/timeline/{}'.format(sdc_repository, build['id'], raw_build_timeline['id']),
-            'build_id': build['id']
+            '_sdc_id': '{}/timeline/{}'.format(sdc_repository, raw_build_timeline['id']),
+            '_sdc_parent_id': '{}/build/{}'.format(sdc_repository, build['id']),
         }
 
         with singer.Transformer() as transformer:
@@ -1198,7 +1199,8 @@ def get_all_builds(schema, org, repo_path, state, mdata, start_date):
                 {
                     **build,
                     '_sdc_repository': sdc_repository,
-                    '_sdc_id': '{}/build/{}'.format(sdc_repository, build['id'])
+                    '_sdc_id': '{}/build/{}'.format(sdc_repository, build['id']),
+                    '_sdc_parent_id': '{}/run/{}'.format(sdc_repository, build['id'])
                 }
                 for build in builds
                 if is_build_after_time(build, bookmarkTime)
